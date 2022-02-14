373e2c6e3f1a0f0b916e0463e88e374f7ebfc723
Anurag Singh <anuragsingh6886@gmail.com>
2022-02-14 14:17:56 +0530

    Updates

---
+++ precache-manifest.16d5717467fbb639a8bdf3944d480108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e719cab46ecc5e6ffb05d7df52f0370",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "fec0b65afed33df7e595",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "bf7550e09bb8dfb65f3b",
+    "url": "/SnapShot/static/js/2.259ad13e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.259ad13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec0b65afed33df7e595",
+    "url": "/SnapShot/static/js/main.5fd89aa6.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
